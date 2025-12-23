dddc4c0c9c92244968487ef2ae439f4b8fdf7bdd
vladicletamir <152497618+vladicletamir@users.noreply.github.com>
2025-12-23 18:08:23 +0100

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -52,6 +52,6 @@ def status():
         "proxima_verificacion": PROXIMA_VERIFICACION
     })
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+if __name__ == "__main__": 
+    port = int(os.environ.get("PORT", "8080")) 
     app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
