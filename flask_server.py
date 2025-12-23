656292a091797789580235e6565ba60e586be02f
vladicletamir <152497618+vladicletamir@users.noreply.github.com>
2025-12-23 18:00:09 +0100

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -53,5 +53,5 @@ def status():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
